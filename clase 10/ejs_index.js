6c4f92acf55bf63a381d8920ca4f6e3cec0c6956
Regina de la Cruz <regis.design22@gmail.com>
2022-07-12 11:37:57 -0500

    Upload clase 10

---
+++ clase 10/ejs_index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+
+const app = express()
+//Configuración EJS --v
+app.set("views", "./views")
+app.set("view engine", "ejs")
+//----+----+----*
+
+app.get("/pets", (req, res) => {
+    const pets = [
+        {name:"Dorado", animal: "pez", months: 12},
+        {name:"Firulais", animal: "perro", months: 20},
+        {name:"Gardel", animal: "perro", months: 10},
+        {name:"Chinchulin", animal: "gato", months: 12},
+        {name:"Michi", animal: "gato", months: 5},
+    ]
+
+    const mensaje = "Mascotas de la clase:"
+    res.render("pets.ejs", {pets, mensaje})
+})
+
+
+app.listen(8080) 
--- clase 10/pug_index.js
+++ clase 10/pug_index.js
@@ -1,10 +1,25 @@
 const express = require("express")
+const { render } = require("pug")
 
 const app = express()
 
+app.set("views", "./views")
+app.set("view engine", "pug") //Definimos cúal va a ser nuestro motor de plantilla, en este caso Pug
+
+app.get("/", (req, res) => {
+    res.render("hello.pug", {msn: "Saludos a todos"})
+})
 app.get("/test", (req, res) => res.send("OK"))
 
-
+app.get("/datos", (req, res) => {
+    res.render("datos.pug", {
+        title: req.query.title,
+        min: req.query.min,
+        max: req.query.max,
+        level: req.query.level
+    })
+})
+//http://127.0.0.1:8080/datos?title=<b>Regina%20de%20la%20Cruz</b>&min=10&max=20&level=18
 
 
 app.listen(8080)
